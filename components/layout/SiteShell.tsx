762419a963f4f2b1c6a02bae29d406a43fd89ff0
Furqon Ramadhani <ahfura70@gmail.com>
2026-08-31 13:03:37 +0800

    refactor: make site shell data-aware

--- components/layout/SiteShell.tsx
+++ components/layout/SiteShell.tsx
@@ -1,10 +1,18 @@
 import Header from './Header'
 import Footer from './Footer'
+import { contentRepository } from '@/lib/data'
+import { buildSearchEntries } from '@/lib/data/presentation'
 
-export default function SiteShell({ children }: { children: React.ReactNode }) {
+export default async function SiteShell({ children }: { children: React.ReactNode }) {
+  const [news, islamic, events] = await Promise.all([
+    contentRepository.listNews(),
+    contentRepository.listIslamic(),
+    contentRepository.listEvents(),
+  ])
+
   return (
     <>
-      <Header />
+      <Header searchItems={buildSearchEntries(news, islamic, events)} />
       {children}
       <Footer />
     </>
