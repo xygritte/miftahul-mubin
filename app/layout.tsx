02cb771cdd1ec60c2a0fa29f433e269bbfb59148
Furqon Ramadhani <ahfura70@gmail.com>
2026-09-08 00:05:00 +0800

    refactor(css): remove redundant stylesheet imports

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,9 +10,7 @@ import './popular-polish.css'
 import './embed-polish.css'
 import './admin.css'
 import './admin-settings.css'
-import './profile-settings.css'
 import './dark-mode-fixes.css'
-import './management-photo-fix.css'
 import './design-tokens.css'
 
 export const metadata: Metadata = {
